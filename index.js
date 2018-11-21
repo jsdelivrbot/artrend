ddc87180a014e9fb54888f5c12acbaadb3b59b21
Max Surgai <public@maxsurgai.com>
2018-11-22 00:30:58 +0200

    Setting app mongoose and / route

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const app = express()
+const path = require('path')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
-mongoose.connect()
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds263172.mlab.com:63172/heroku_376zbtqs')
+const db = mongoose.connection
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
 
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', function(req, res){
+      res.send('Hello, Ruben')
+})
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
