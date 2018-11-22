b6b4b7fa9c58c5c1d95e2c25cd7dd0a5a5f49a71
Max Surgai <public@maxsurgai.com>
2018-11-22 20:12:05 +0200

    Small changes

--- index.js
+++ index.js
@@ -13,39 +13,37 @@ User = require('./models/user');
 mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds263172.mlab.com:63172/heroku_376zbtqs');
 const db = mongoose.connection;
 
-app
-    // .use(express.static(path.join(__dirname, 'public')))
+//app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', function(req, res){
+    res.send("/api/users<br/>" +
+        "/api/projects\n<br/>" +
+        "/api/models\n<br/>" +
+        "/api/surfaces\n<br/>" +
+        "/api/trends\n<br/>" +
+        "/api/textures\n<br/>" +
+        "/api/scenes\n<br/>" +
+        "/api/environments\n<br/>" +
+        "/api/lightSetups");
+});
 
-    .get('/', function(req, res){
-        res.send("/api/users<br/>" +
-            "/api/projects\n<br/>" +
-            "/api/models\n<br/>" +
-            "/api/surfaces\n<br/>" +
-            "/api/trends\n<br/>" +
-            "/api/textures\n<br/>" +
-            "/api/scenes\n<br/>" +
-            "/api/environments\n<br/>" +
-            "/api/lightSetups");
+app.get('/api/users', function (req, res) {
+    console.log('Route get request');
+    User.getUsers(function (err, users) {
+        if (err) {
+            throw err;
+        }
+        res.json(users);
     })
+});
 
-    .get('/api/users', function (req, res) {
-        console.log('Route get request');
-        User.getUsers(function (err, users) {
-            if (err) {
-                throw err;
-            }
-            res.json(users);
-        })
+app.post('/api/users', function (req, res) {
+    const user = req.body;
+    User.addUser(user, function (err, user) {
+        if (err) {
+            throw err;
+        }
+        res.json(user);
     })
+});
 
-    .post('/api/users', function (req, res) {
-        const user = req.body;
-        User.addUser(user, function (err, user) {
-            if (err) {
-                throw err;
-            }
-            res.json(user);
-        })
-    })
-
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
