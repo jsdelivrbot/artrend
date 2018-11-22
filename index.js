23aa87f710ce8400e06dc032df93ad0247780883
Max Surgai <public@maxsurgai.com>
2018-11-22 19:00:58 +0200

    User Schema

--- index.js
+++ index.js
@@ -5,14 +5,33 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
+User = require('./models/user')
+
 mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds263172.mlab.com:63172/heroku_376zbtqs')
 const db = mongoose.connection
 
 app
-  .use(express.static(path.join(__dirname, 'public')))
+    .use(express.static(path.join(__dirname, 'public')))
 
-  .get('/', function(req, res){
-      res.send('Hello, Ruben')
-})
+    .get('/', function(req, res){
+        res.send("/api/users<br/>" +
+            "/api/projects\n<br/>" +
+            "/api/models\n<br/>" +
+            "/api/surfaces\n<br/>" +
+            "/api/trends\n<br/>" +
+            "/api/textures\n<br/>" +
+            "/api/scenes\n<br/>" +
+            "/api/environments\n<br/>" +
+            "/api/lightSetups")
+    })
 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .get('/api/users', function (req, res) {
+        User.getUsers(function (err, users) {
+            if (err) {
+                throw  err;
+            }
+            res.json(users)
+        })
+    })
+
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ models/user.js
@@ -0,0 +1,29 @@
+/*
+ * Project: artrend
+ * Author: Max Surgai / maxsurgai.com
+ * Copyright (c) 2018.
+ */
+
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+
+    _id: {
+        type: mongoose.Schema.ObjectId,
+        auto: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const User = module.exports = mongoose.model('User', userSchema)
+
+module.exports.getUsers = function (callback, limit) {
+    User.find(callback). limit(limit);
+}
